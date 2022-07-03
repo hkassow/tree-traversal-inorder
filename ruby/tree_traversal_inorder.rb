3ef2ef5dae88a8eb4b2258d8741581358d963230
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-03 11:23:30 -0400

    ruby bst traversal inorder

--- ruby/tree_traversal_inorder.rb
+++ ruby/tree_traversal_inorder.rb
@@ -1,3 +1,4 @@
+#require 'pry'
 class Node
   attr_accessor :value, :left, :right
 
@@ -9,22 +10,38 @@ class Node
 end
 
 def tree_traversal_inorder(root)
-  # type your code in here
+  #add values to array from a binary search tree inorder
+  array = []  
+  return array if root.nil?
+  
+  stack = [root]
+  while !stack.empty?
+    #top  => stack[-1]
+    if ! stack[-1].left.nil? 
+      #cut off left branch path
+      #add left branch to top of stack
+      #since left branches are smaller dont add any values to array
+      left_sub = stack[-1].left
+      stack[-1].left = nil
+      stack.push(left_sub)
+    else  
+      #no left branch => smallest value now
+      #remove it from stack
+      #add it to array 
+      popped = stack.pop
+      array.push popped.value
+      stack.push popped.right if ! popped.right.nil?
+    end
+  end
+  array
 end
-
+root = Node.new(2, Node.new(-10), Node.new(20))
+puts "Expecting: [-10, 2, 20]"
+pp tree_traversal_inorder(root)
 if __FILE__ == $PROGRAM_NAME
-  root = Node.new(2, Node.new(-10), Node.new(20))
-  puts "Expecting: [-10, 2, 20]"
-  print tree_traversal_inorder(root)
-  puts
-
-  puts
-
-  root = Node.new(10, Node.new(0, nil, Node.new(5)), Node.new(20, nil, Node.new(30)))
-  puts "Expecting: [0, 5, 10, 20, 30] "
-  print tree_traversal_inorder(root)
-  puts
-
+  #root = Node.new(10, Node.new(0, nil, Node.new(5)), Node.new(20, nil, Node.new(30)))
+  #puts "Expecting: [0, 5, 10, 20, 30] "
+  #tree_traversal_inorder(root)
   # Don't forget to add your own!
 end
 
